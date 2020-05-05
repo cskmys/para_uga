ea64d1645776ee5824812da135a2b03c07f7e894
csk <cskmys@gmail.com>
2020-05-05 14:24:51 +0200

    matrix product in virtual done

--- l5prep/mpiscatteringmatrix/main.c
+++ l5prep/mpiscatteringmatrix/main.c
@@ -21,7 +21,7 @@
 #define NB_ELE(x) sizeof(x)/ELE_SIZ(x)
 
 #define DIM_A 4
-#define COL_B 1
+#define COL_B 4
 
 int getIdx(int nbEle, int idx){
     idx = idx % nbEle;
@@ -68,7 +68,7 @@ void reduce(int size, int rank){
     int rSA = rA / p;
     int cSA = cA;
     mat *sub_A = allocMatMem(rSA, cSA);
-    MPI_Scatter(A, rSA * cSA, MPI_DOUBLE, sub_A, rSA * cSA, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(A, (rA / p) * cA, MPI_DOUBLE, sub_A, rSA * cSA, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     if(rank == 0){
         free(A);
     }
@@ -87,83 +87,83 @@ void reduce(int size, int rank){
     mpi_printf("sub_B:\n");
     printMatrix(sub_B, rSB, cSB);
 
-    // int rSC = rC / p;
-    // int cSC = cC;
-    // mat *sub_C = allocMatMem(rSC, cSC);
-    // memsetMatrix(sub_C, 0, rSC, cSC);
+    int rSC = rC / p;
+    int cSC = cC;
+    mat *sub_C = allocMatMem(rSC, cSC);
+    memsetMatrix(sub_C, 0, rSC, cSC);
 
-    // for(int step = 0; step < p; ++step){
-    //     int rSAA = rSA;
-    //     int cSAA = rSB;
-    //     mat *sub_A_A = allocMatMem(rSAA, cSAA);
-    //     for (int i = 0; i < rSAA; ++i){
-    //         for (int j = 0; j < cSAA; ++j){
-    //             int idx = getIdx( cSA, (((rank - step) % p) * cSAA) + j);
-    //             int sub_arr_idx = (i * cSA) + idx;
-    //             sub_A_A[(i * rSAA) + j] = sub_A[sub_arr_idx];
-    //         }
-    //     }
-    //     mpi_printf("sub_A_A:\n");
-    //     printMatrix(sub_A_A, rSAA, cSAA);
+    for(int step = 0; step < p; ++step){
+        int rSAA = rSA;
+        int cSAA = rSB;
+        mat *sub_A_A = allocMatMem(rSAA, cSAA);
+        for (int i = 0; i < rSAA; ++i){
+            for (int j = 0; j < cSAA; ++j){
+                int idx = getIdx( cSA, (((rank - step) % p) * cSAA) + j);
+                int sub_arr_idx = (i * cSA) + idx;
+                sub_A_A[(i * rSAA) + j] = sub_A[sub_arr_idx];
+            }
+        }
+        mpi_printf("sub_A_A:\n");
+        printMatrix(sub_A_A, rSAA, cSAA);
 
-    //     int rSCS = rSAA;
-    //     int cSCS = cSB;
-    //     mat *sub_C_Step = allocMatMem(rSCS, cSCS);
+        int rSCS = rSAA;
+        int cSCS = cSB;
+        mat *sub_C_Step = allocMatMem(rSCS, cSCS);
 
-    //     matrixMul(sub_A_A, rSAA, cSAA, sub_B, rSB, cSB, sub_C_Step);
-    //     mpi_printf("sub_C_Step:\n");
-    //     printMatrix(sub_C_Step, rSCS, cSCS);
+        matrixMul(sub_A_A, rSAA, cSAA, sub_B, rSB, cSB, sub_C_Step);
+        mpi_printf("sub_C_Step:\n");
+        printMatrix(sub_C_Step, rSCS, cSCS);
 
-    //     mpi_printf("cur sub_C:\n");
-    //     printMatrix(sub_C, rSC, cSC);
-    //     sumMatrix(sub_C, sub_C_Step, rSC, cSC, sub_C);
-    //     mpi_printf("after sum sub_C:\n");
-    //     printMatrix(sub_C, rSC, cSC);
-    //     free(sub_C_Step);
-    //     free(sub_A_A);
+        mpi_printf("cur sub_C:\n");
+        printMatrix(sub_C, rSC, cSC);
+        sumMatrix(sub_C, sub_C_Step, rSC, cSC, sub_C);
+        mpi_printf("after sum sub_C:\n");
+        printMatrix(sub_C, rSC, cSC);
+        free(sub_C_Step);
+        free(sub_A_A);
         
-    //     if(rank == 0){
+        if(rank == 0){
 
-    //         ring_tx_chunk(size, rank, sub_B, rSB * cSB);
+            ring_tx_chunk(size, rank, sub_B, rSB * cSB);
             
-    //         int rxCnt;
-    //         ring_rx_chunk(size, rank, sub_B, &rxCnt);
-    //         assert(rxCnt == rSB * cSB);
+            int rxCnt;
+            ring_rx_chunk(size, rank, sub_B, &rxCnt);
+            assert(rxCnt == rSB * cSB);
 
-    //         mpi_printf("Rxed:\n");
-    //         printMatrix(sub_B, rSB, cSB);
+            mpi_printf("Rxed:\n");
+            printMatrix(sub_B, rSB, cSB);
 
-    //     } else {
-    //         mat *rxBuff = allocMatMem(rSB, cSB);
-    //         int rxCnt;
-    //         ring_rx_chunk(size, rank, rxBuff, &rxCnt);
-    //         assert(rxCnt == rSB * cSB);
+        } else {
+            mat *rxBuff = allocMatMem(rSB, cSB);
+            int rxCnt;
+            ring_rx_chunk(size, rank, rxBuff, &rxCnt);
+            assert(rxCnt == rSB * cSB);
             
-    //         mpi_printf("Rxed:\n");
-    //         printMatrix(rxBuff, rSB, cSB);
+            mpi_printf("Rxed:\n");
+            printMatrix(rxBuff, rSB, cSB);
 
-    //         ring_tx_chunk(size, rank, sub_B, rSB * cSB);
+            ring_tx_chunk(size, rank, sub_B, rSB * cSB);
 
-    //         free(sub_B);
-    //         sub_B = rxBuff;
-    //     }
-    // }
+            free(sub_B);
+            sub_B = rxBuff;
+        }
+    }
 
 
-    // mpi_printf("sub_C:\n");
-    // printMatrix(sub_C, rSC, cSC);
-    // free(sub_A);
-    // free(sub_B);
+    mpi_printf("sub_C:\n");
+    printMatrix(sub_C, rSC, cSC);
+    free(sub_A);
+    free(sub_B);
     
-    // // MPI_Gather(sub_C, rSC * cSC, MPI_DOUBLE, C, rSC * cSC, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // // free(sub_C);
+    MPI_Gather(sub_C, rSC * cSC, MPI_DOUBLE, C, rSC * cSC, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    free(sub_C);
 
-    // if(rank == 0){        
-    //     // mpi_printf("matC:\n");
-    //     // printMatrix(C, rC, cC);
+    if(rank == 0){        
+        mpi_printf("matC:\n");
+        printMatrix(C, rC, cC);
 
-    //     free(C);
-    // }
+        free(C);
+    }
     return;
 }
 
